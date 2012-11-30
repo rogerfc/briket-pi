41632ec35f62eac2b0338fe82a5292437108465a
Roger Firpo Curcoll <roger.firpo@gmail.com>
2012-11-30 20:41:27 +0100

    Add fakeGPIO module to be able to work on briket module before Laia provides with a hardware solution

---
+++ src/fakeGPIO.py
@@ -0,0 +1,19 @@
+BOARD	= None
+IN		= None
+OUT		= None
+
+inputfile = 'fakeniput.yaml'
+
+def setmode(dummy):
+	pass
+
+def setup(port, dummy):
+	pass
+	
+def input(port):
+	return yaml.safe_load(open('ioconfig.yaml'))[port]
+	# return True
+	
+def output(port, value = True):
+	print 'setting output port %s to %s' % (port, value)
+	
